f3a18f20b76dd05faca601d060f6214e588e3e82
Daniel Schöttler <schoettler.dan@gmail.com>
2023-01-18 15:38:21 +0100

    A3.2 Aufgabe 1 Kreis

---
+++ src/Kreis.java
@@ -0,0 +1,13 @@
+public class Kreis {
+    public static void main(String[] args){
+
+       String stern = "*";
+       
+        System.out.printf("%7s", stern + stern);
+        System.out.printf("\n %1s %8s", stern ,stern); 
+        System.out.printf("\n %1s %8s", stern ,stern); 
+        System.out.printf("\n %6s", stern + stern);
+       
+        
+}
+}
